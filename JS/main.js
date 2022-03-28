0dd314b630af4b8dc2536ac1a03874916511265b
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-03-28 18:19:50 -0400

    Additions to JS Project

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+window.alert("Hello, World!"); //alert pop up that says hello world
+document.write("Hello, World!"); //
+var A= "This is a string <br>"; //assigning variable A
+document.write(A); //Request for the variable to be printed 
+var B="This is a second string <br>"; //defining variable B
+document.write(B); //request for print of variable B
+document.write("Lisa told Bart, \"Knock it off Bart! or I'll tell dad!\" <br> \"Eat my shorts!\" Bart Responded.<br>");//string using quotation marks
+document.write("\"Be who you are and say what you feel,"
++"because those who mind don\'t matter and those who matter don\'t mind.\""
++"-Dr. Suess <br>"); //concatenating a string
+var B= "Concatenated" + "string";
+document.write(B)
+var Family="The Baileys", Dad="Sedrick", Mom="Shalena", Son="Rico"; //assigning multiple variables in one statement.
+document.write(Son); //Calling the son variable
+document.write(3+3); //funtion of 3+3
+var str="This is a green color!"; 
+var result= str.fontcolor("green"); //attempt to establish green color font using fontcolor method
+let text="This is a green color!";
+let result= text.fontcolor("green"); //attempt to establish green color font using fontcolor method
+String.fontcolor("green"); //attempt to establish green color
