2d3fd8204df9460453f0e2b0082d100573289a47
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-03-29 11:12:55 -0400

    Practice of the function and += Operator in JS

---
+++ Project2_Functions/JS/main.js
@@ -0,0 +1,11 @@
+function My_First_Function() {
+    var str="This is the button text!";
+    document.getElementById("Button_Text").innerHTML=str;
+}
+
+function MyFunction() {
+    var sentence="I am learning";
+    sentence +="a lot from this book!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+
+}
