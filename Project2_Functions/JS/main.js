1eeff6ea13d3b8d0ceb0b3f45ad6dd91c0489d57
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-03-29 11:16:20 -0400

    Update main.js

--- Project2_Functions/JS/main.js
+++ Project2_Functions/JS/main.js
@@ -1,11 +1,11 @@
-function My_First_Function() {
-    var str="This is the button text!";
-    document.getElementById("Button_Text").innerHTML=str;
+function My_First_Function() {  //Defining and naming function//
+    var str="This is the button text!"; //definf the variable and giving it a string value//
+    document.getElementById("Button_Text").innerHTML=str;  //putting the value of the variable into the HTML element frompoint with the "Button_Text" id.//
 }
 
-function MyFunction() {
-    var sentence="I am learning";
-    sentence +="a lot from this book!";
-    document.getElementById("Concatenate").innerHTML=sentence;
+function MyFunction() { //Defining and nameing the function.
+    var sentence="I am learning"; //defining the variable
+    sentence +="a lot from this book!"; //confinung the variable using the += operator
+    document.getElementById("Concatenate").innerHTML=sentence; //putting the value of the variable into the HTML element
 
 }
