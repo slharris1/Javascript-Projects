8e9ebedc677965926312e0697f32efc32fc9f603
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-04-03 19:50:47 -0400

    Project 10

---
+++ Project10_loops_arrays/JS/main.js
@@ -0,0 +1,67 @@
+function call_Loop() {
+    var Digit= "";
+    var X=1;
+    while (X<11) {
+        Digit += "<br>" + X;
+        X++
+    }
+    document.getElementById("Loop").innerHTML= Digit;
+    
+}
+
+function call_String() {
+  let text= "Hello World!";
+  let length= text.length;
+    document.getElementById("Length").innerHTML= length;
+}
+
+var Instruments= ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content= "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br";
+    }
+    document.getElementById("List_of_instruments").innerHTML= Content;
+}
+
+function vegetable_List() {
+    var vegetable_List= [];
+    vegetable_List [0]= "Carrot";
+    vegetable_List [1]= "Pepper";
+    vegetable_List [2]= "Cucumber";
+    vegetable_List [3]= "Broccoli";
+    document.getElementById("Vegetables").innerHTML = "In this list, the only green vegetable is " + vegetable_List[3] + ".";
+}
+
+function constant_Function() {
+    const Musical_Instruments= {type: "violin", brand: "electric", color: "white"};
+    Musical_Instruments.price= "$11,000";
+    Musical_Instruments.type= "cello";
+    document.getElementById("Constant").innerHTML= "The cost of the " + Musical_Instruments.type + " is listed at a whopping " + Musical_Instruments.price + "!!!";
+}
+
+var A= 204;
+document.write(A);
+{
+    let B= 100;
+    document.write("<br>" + B);
+}
+var C= 8000;
+document.write("<br>" + C);
+
+function my_Function(name) {
+    return "Hello" + name;
+    document.getElementById("demo").innerHTML= my_Function("Shalena");
+}
+
+let animal = {
+    Breed: "Cat",
+    Age: "2",
+    Color: "white",
+    disposition: "energetic",
+    description: function() {
+        return "Rico is a " + this.Age + " year old " + this.disposition + ", " + this.Color + this.Breed;
+    }
+};
+document.getElementById("animal").innerHTML= animal.description();
